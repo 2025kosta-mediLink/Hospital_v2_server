766640b2a094960c76cfe842928695946ba2f302
imeline <iseonju745@gmail.com>
2025-10-31 17:33:34 +0900

    init: uuid 추출, user 추출 로직 생성

---
+++ src/main/java/medlink/member/service/MemberService.java
@@ -0,0 +1,22 @@
+package medlink.member.service;
+
+import lombok.RequiredArgsConstructor;
+import medlink.common.exception.ErrorStatus;
+import medlink.common.exception.GlobalException;
+import medlink.member.entity.Member;
+import medlink.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private MemberRepository memberRepository;
+
+    public Member getByUuid(String uuid) {
+        return memberRepository.findByUuid(uuid)
+                .orElseThrow(() -> new GlobalException(ErrorStatus.MEMBER_NOT_FOUND));
+    }
+}
