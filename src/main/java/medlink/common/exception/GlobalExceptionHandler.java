43abe9d268d59b74aaa7cc39f3a8a9d7ad201050
imeline <iseonju745@gmail.com>
2025-11-12 12:04:37 +0900

    feat: 로그인 상태인지 검증 로직, 전역 설정 (auth url 제외)

--- src/main/java/medlink/common/exception/GlobalExceptionHandler.java
+++ src/main/java/medlink/common/exception/GlobalExceptionHandler.java
@@ -1,10 +1,9 @@
 package medlink.common.exception;
 
-import medlink.common.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import medlink.common.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
@@ -15,10 +14,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
-
-    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
     /**
      * 비즈니스 예외 처리 (커스텀 4xx 등)
      */
