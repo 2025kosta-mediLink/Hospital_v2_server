2708619743ad28d4826e96bedac5458004d8c15c
minji-yoon <ymj0837@gmail.com>
2025-11-18 12:24:17 +0900

    refactor: 지도 연결

---
+++ src/main/java/medlink/pharmacy/dto/request/PharmacySendRequest.java
@@ -0,0 +1,17 @@
+package medlink.pharmacy.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PharmacySendRequest {
+	private String pharmacyId;
+	private List<Long> prescriptionIds;
+}
